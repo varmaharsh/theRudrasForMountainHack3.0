d7560ed1482d690a6bfccbf976077a0f5a1830a7
varmaharsh <varmaharsh15@gmail.com>
2022-09-18 16:03:42 +0530

    all contestant changes

--- webapp/Components/contestants.js
+++ webapp/Components/contestants.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button } from "web3uikit";
 // import './Users.css'
+import Contestant from "../Components/contestant";
 
-const AllContestants = ({ allCandidates }) => {
-  return (
+const AllContestants = ({
+  allCandidates,
+  getCandidates,
+  candidateDetails,
+  promisesByCandidateId,
+  addPromise,
+  isACandidate,
+}) => {
+  const [showCandidatePage, setShowCandidatePage] = useState(false);
+
+  console.log(allCandidates);
+  return showCandidatePage ? (
+    <div>
+      <Button
+        onClick={() => {
+          setShowCandidatePage(false);
+          getCandidates();
+        }}
+        text="Back"
+        type="button"
+      />
+      <Contestant
+        candidateDetails={candidateDetails}
+        promisesByCandidateId={promisesByCandidateId}
+        addPromise={addPromise}
+        isACandidate={isACandidate}
+      />
+    </div>
+  ) : (
     <div>
       <h1>CONTESTANTS</h1>
       {/* <Promise/> */}
       {allCandidates.map((acandi, index) => {
-        // const net = `${acandi.split(',')}`}
-        console.log('trying', `${acandi}`.split(',')[4]);
-        // console.log("trying",{acandi})
+        console.log("trying", `${acandi}`.split(",")[4]);
         return (
-          <div key={index} className="promise-list">
+          <div
+            key={index}
+            className="promise-list"
+            onClick={() => {
+              getCandidates(acandi.id);
+              setShowCandidatePage(true);
+            }}
+          >
             <p>NAME:{acandi.name}</p>
             <p>ID: {acandi.id}</p>
             <p>CONSTITUENCY: {acandi.constituency}</p>
-            <p>NETWORTH: {`${acandi}`.split(',')[4]}</p>
+            <p>NETWORTH: {`${acandi}`.split(",")[4]}</p>
             <p>PARTY: {acandi.party}</p>
           </div>
         );
