a9f679462a71d33a23f08edfb063f5c517e220e7
varmaharsh <varmaharsh15@gmail.com>
2022-09-18 14:26:37 +0530

    add promise modal changes

--- webapp/Components/modal.js
+++ webapp/Components/modal.js
@@ -1,24 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNotification } from "web3uikit";
 
-function modal() {
-    const [Domain, setDomain] = useState("");
-    const [Description, setDescription] = useState("");
+function modal({ addPromise, setShowModal }) {
+  const [Domain, setDomain] = useState("");
+  const [Description, setDescription] = useState("");
+  const dispatch = useNotification();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(`The domain you entered was: ${Domain}`)
-      }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const success = await addPromise(Domain, Description);
+    if (success) {
+      dispatch({
+        type: "success",
+        message: "Successfully added promise to your election manifesto",
+        title: "Promise Added",
+        position: "topR",
+      });
+    } else {
+      dispatch({
+        type: "error",
+        message:
+          "Try again or check if you're signed in with the correct account",
+        title: "Promise Couldn't be added",
+        position: "topR",
+      });
+    }
+    setShowModal(false);
+    setDomain("");
+    setDescription("");
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>
           Domain:
-          <input type="text" name="Domain" onChange={(e) => setDomain(e.target.value)}/>
+          <input
+            type="text"
+            name="Domain"
+            required={true}
+            onChange={(e) => setDomain(e.target.value)}
+          />
         </label>
         <label>
           Description:
-          <input type="text" name="Description" onChange={(e) => setDomain(e.target.value)}/>
+          <input
+            type="text"
+            name="Description"
+            required={true}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
